6db62625ad704c1cd0c985febf2ad19a6b161bb4
Michał Niedźwiedzki <michal@epsi.pl>
2020-02-20 20:51:05 +0100

    Created command line and composer setup

---
+++ cli.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+
+require "input.php";
+
+// check if argument passed
+if ($argc < 2) {
+    fwrite(STDERR, "Expected 1 argument\n");
+    exit -1;
+}
+
+// check if file exists
+$input = $argv[1];
+if (!file_exists($input)) {
+    fwrite(STDERR, "File {$input} not found\n");
+    exit -1;
+}
+
+// load input file
+$scanning = Scanning::load($input);
+
