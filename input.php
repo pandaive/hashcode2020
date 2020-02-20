35f83e3d362a99305b4a6f2529ddc2b6e95dc619
Michał Niedźwiedzki <michal@epsi.pl>
2020-02-20 20:37:37 +0100

    Input

---
+++ input.php
@@ -0,0 +1,50 @@
+<?php
+
+class Library {
+
+    public $onboardingTime = 0;
+    public $booksPerDay = 0;
+    public $bookIds = [];
+
+}
+
+class Scanning {
+
+    public $days = 0;
+
+    public $booksToScores = [];
+    public $libraries = [];
+
+    const DELIMITER = " ";
+
+    public function __construct(integer $days, array $booksToScores, array $libraries) {
+        $this->days = $days;
+        $this->booksToScored = $booksToScores;
+        $this->libraries = $libraries;
+    }
+
+    public static function load(string $file): Scanning {
+        // init
+        $h = fopen($file, "r");
+
+        // load books count, libraries count, days
+        list($booksCount, $librariesCount, $days) = fscanf($h, "%d %d %d");
+
+        // load scores
+        $booksToScores = fgetcsv($h, 0, self::DELIMITER);
+
+        // load libraries
+        $libraries = [];
+        while (!feof($h)) {
+            list($booksInLibraryCount, $onboardingTime, $booksPerDay) = fgetcsv($h, "%d %d %d");
+            $booksInLibrary = fgetcsv($h, 0, self::DELIMITER);
+            if ($booksInLibraryCount !== count($booksInLibrary)) {
+                throw new Exception("Reading library expected to get {$booksInLibraryCount} books but read {count($booksInLibrary)} items");
+            }
+            $libraries[] = new Library($booksInLibrary, $onboardingTime, $booksPerDay);
+        }
+
+        return new Scanning($days, $booksToScores, $libraries);
+    }
+
+}
